21df79585b89cb5a8b81c2b03e3af5e77f107840
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-16 21:17:13 +0900

    Add getPost to PostDetail

--- src/Components/Posts/PostDetail.js
+++ src/Components/Posts/PostDetail.js
@@ -2,11 +2,28 @@ import React from 'react';
 import '../../Design/Post.css';
 
 class PostDetail extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {}
+    }
+  }
+  componentWillMount () {
+    this.getPosts()
+  }
+  getPosts = () => {
+    const id = this.props.match.params.id
+    fetch(`https://k-blog0130.herokuapp.com/en/api/v1/posts/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({data:data.data})
+    })
+  }
   render() {
     return (
       <div>
-        <h3>test</h3>
-        {console.log(this.props.match.params.id)}
+        <h3>{this.state.data.title}</h3>
+        <p>{this.state.data.context}</p>
       </div>
     )
   }
