c7f09dd9386c13772bebe394358e98358194b6f6
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-27 17:34:39 +0900

    Updates

--- precache-manifest.e155f454320bb9756c3d06c92ae6da0d.js
+++ precache-manifest.e155f454320bb9756c3d06c92ae6da0d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/MyBlogFront/static/js/runtime~main.c6c3dbe7.js"
   },
   {
-    "revision": "808dd9cb22944bcc3924",
-    "url": "/MyBlogFront/static/js/main.808dd9cb.chunk.js"
+    "revision": "2f541ae9249cc581f055",
+    "url": "/MyBlogFront/static/js/main.2f541ae9.chunk.js"
   },
   {
     "revision": "5391467cb0cea8699329",
     "url": "/MyBlogFront/static/js/1.5391467c.chunk.js"
   },
   {
-    "revision": "808dd9cb22944bcc3924",
-    "url": "/MyBlogFront/static/css/main.b54d525f.chunk.css"
+    "revision": "2f541ae9249cc581f055",
+    "url": "/MyBlogFront/static/css/main.73988cc7.chunk.css"
   },
   {
-    "revision": "2ae4ed672fb1e8a60a79782280587acb",
+    "revision": "475f9e56378e49f4aaacc64886e60083",
     "url": "/MyBlogFront/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyBlogFront/precache-manifest.263775f8d3df0f207b16f14e5c525900.js"
+  "/MyBlogFront/precache-manifest.e155f454320bb9756c3d06c92ae6da0d.js"
 );
 
 workbox.clientsClaim();
