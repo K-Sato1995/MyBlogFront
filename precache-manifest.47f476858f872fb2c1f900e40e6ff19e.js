530c15f39387650b34c4b9f68a2bb165b24735f8
Katsuki <Katsuki_Sato0130@icloud.com>
2019-02-04 19:49:47 +0900

    Updates

---
+++ precache-manifest.47f476858f872fb2c1f900e40e6ff19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15336026e57ce1d1e68c3d468b4e8cc",
+    "url": "/MyBlogFront/static/media/penguin.b1533602.png"
+  },
+  {
+    "revision": "c6c3dbe79b4837e55536",
+    "url": "/MyBlogFront/static/js/runtime~main.c6c3dbe7.js"
+  },
+  {
+    "revision": "cb8db2e3b7f7ccf1b5b0",
+    "url": "/MyBlogFront/static/js/main.cb8db2e3.chunk.js"
+  },
+  {
+    "revision": "23ab04d030b85814e9d6",
+    "url": "/MyBlogFront/static/js/1.23ab04d0.chunk.js"
+  },
+  {
+    "revision": "cb8db2e3b7f7ccf1b5b0",
+    "url": "/MyBlogFront/static/css/main.7faf470c.chunk.css"
+  },
+  {
+    "revision": "361fae52e752bcd12972508d865afae8",
+    "url": "/MyBlogFront/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyBlogFront/precache-manifest.7e745c4cf799d6ce37136206ebb71789.js"
+  "/MyBlogFront/precache-manifest.47f476858f872fb2c1f900e40e6ff19e.js"
 );
 
 workbox.clientsClaim();
