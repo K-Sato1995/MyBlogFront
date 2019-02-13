43796aca460439fce21c009d057a25784330d8c1
Katsuki <Katsuki_Sato0130@icloud.com>
2019-02-14 07:26:44 +0900

    Add Tab Contact

---
+++ src/Components/About/RightPart/Tabs/TabContact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TabContact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>TabContact</h1>
+      </div>
+    )
+  }
+}
+
+export default TabContact;
--- src/Components/About/RightPart/Tabs/TabMain.js
+++ src/Components/About/RightPart/Tabs/TabMain.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TabIntro from './TabIntro';
 import TabWork from './TabWork';
 import TabButton from './TabButton';
+import TabContact from './TabContact';
 
 class TabMain extends React.Component {
   constructor() {
@@ -11,6 +12,7 @@ class TabMain extends React.Component {
     }
     this.activeWork = this.activeWork.bind(this);
     this.activeIntro = this.activeIntro.bind(this);
+    this.activeContact = this.activeContact.bind(this);
   }
   activeIntro () {
     this.setState ({ activeTab: 1 })
@@ -18,12 +20,18 @@ class TabMain extends React.Component {
   activeWork () {
     this.setState ({ activeTab: 2 })
   }
+  activeContact() {
+    this.setState ({activeTab: 3})
+  }
+
   render() {
     let ActiveTab;
     if (this.state.activeTab === 1) {
       ActiveTab = <TabIntro />
     }else if (this.state.activeTab === 2) {
       ActiveTab = <TabWork />
+    }else if (this.state.activeTab === 3) {
+      ActiveTab = <TabContact />
     }
 
     return (
@@ -31,6 +39,7 @@ class TabMain extends React.Component {
         <h1>TabMain</h1>
         <TabButton changeTab = {this.activeIntro} name='About'/>
         <TabButton changeTab = {this.activeWork} name='Work'/>
+        <TabButton changeTab = {this.activeContact} name='Contact'/>
         { ActiveTab }
       </div>
     )
