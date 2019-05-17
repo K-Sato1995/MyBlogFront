b0594806b4aa1870ab01205a189a78dfe507ea67
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-17 20:05:34 +0900

    Updates

---
+++ precache-manifest.4d3ec3e7ef7b4e0692b3679900bd492c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15336026e57ce1d1e68c3d468b4e8cc",
+    "url": "/k_blog/static/media/penguin.b1533602.png"
+  },
+  {
+    "revision": "dd4ade1c439aef4ce037013725e1681d",
+    "url": "/k_blog/static/media/gopher_me.dd4ade1c.png"
+  },
+  {
+    "revision": "b10bd9a3698e9deda9e0",
+    "url": "/k_blog/static/js/runtime~main.b10bd9a3.js"
+  },
+  {
+    "revision": "fb404de942e93bd998e4",
+    "url": "/k_blog/static/js/main.fb404de9.chunk.js"
+  },
+  {
+    "revision": "85631c2dd46af0faf464",
+    "url": "/k_blog/static/js/1.85631c2d.chunk.js"
+  },
+  {
+    "revision": "fb404de942e93bd998e4",
+    "url": "/k_blog/static/css/main.2c17d144.chunk.css"
+  },
+  {
+    "revision": "d2deb768963b00448d2d0812b25cb831",
+    "url": "/k_blog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyBlogFront/precache-manifest.fe275c29492b00ac07a08df75c94bd05.js"
+  "/k_blog/precache-manifest.4d3ec3e7ef7b4e0692b3679900bd492c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MyBlogFront/index.html", {
+workbox.routing.registerNavigationRoute("/k_blog/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
