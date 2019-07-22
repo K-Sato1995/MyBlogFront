45740ad4fde57efe141e11fd7d31cf2d86af2988
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-22 21:11:42 +0900

    Add trimString && Delete console.log

--- src/Components/Nav/SideNav/Bottom/Lists/FeaturedPostList/List/Item/index.js
+++ src/Components/Nav/SideNav/Bottom/Lists/FeaturedPostList/List/Item/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { trimString } from "../../../../../../../../MiddleWares/Renderers/Data";
 class FeaturedPost extends React.Component {
   render() {
     const { slug, title } = this.props;
+    const featuredPostTitle = trimString(title, 30);
     return (
       <Link to={`/Post/${slug}`} className="featured-post-title">
-        {title}
+        {featuredPostTitle}
       </Link>
     );
   }
