7fbd9fe1cd2f4986c9b247233db79b373cac15ed
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-05 05:36:26 +0900

    Add API V2

---
+++ src/MiddleWares/Api/V1/index.js
@@ -0,0 +1,40 @@
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://k-blog0130.herokuapp.com/";
+
+export async function getPosts() {
+  let response = await fetch(`${baseUrl}/api/v1/posts`);
+  let data = await response.json();
+  return data;
+}
+
+export async function getPost(slug) {
+  let response = await fetch(`${baseUrl}api/v1/posts/${slug}`);
+  let data = await response.json();
+  return data;
+}
+
+export async function addLike(slug) {
+  let response = await fetch(`${baseUrl}/api/v1/posts/${slug}/like`, {
+    method: "PUT"
+  });
+  let data = await response.json();
+  return data;
+}
+
+export async function createComment(slug, name, content) {
+  let response = await fetch(`${baseUrl}/api/v1/posts/${slug}/comments`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      content: content
+    })
+  });
+  let data = await response.json();
+  return data;
+}
---
+++ src/MiddleWares/Api/V2/index.js
@@ -0,0 +1,58 @@
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://k-blog0130.herokuapp.com/";
+
+export async function getPosts() {
+  let response = await fetch(`${baseUrl}/api/v2/posts`);
+  let data = await response.json();
+  return data;
+}
+
+export async function getFeaturedPosts() {
+  let response = await fetch(`${baseUrl}api/v2/featured_posts`);
+  let data = await response.json();
+  return data;
+}
+
+export async function getPost(slug) {
+  let response = await fetch(`${baseUrl}api/v2/posts/${slug}`);
+  let data = await response.json();
+  return data;
+}
+
+export async function getCategories() {
+  let response = await fetch(`${baseUrl}api/v2/categories`);
+  let data = await response.json();
+  return data;
+}
+
+export async function getTags() {
+  let response = await fetch(`${baseUrl}api/v2/tags`);
+  let data = await response.json();
+  return data;
+}
+
+export async function addLike(slug) {
+  let response = await fetch(`${baseUrl}/api/v1/posts/${slug}/like`, {
+    method: "PUT"
+  });
+  let data = await response.json();
+  return data;
+}
+
+export async function createComment(slug, name, content) {
+  let response = await fetch(`${baseUrl}/api/v1/posts/${slug}/comments`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      content: content
+    })
+  });
+  let data = await response.json();
+  return data;
+}
